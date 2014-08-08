a7791b2350e68305d375909d879d881efc1e90dc
Jeremy <jeremy@kwmassage.com>
2014-08-08 15:33:57 -0400

    hw1-3 work

--- hw1/ruby-calisthenics/lib/rock_paper_scissors.rb
+++ hw1/ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -4,7 +4,46 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    p1s = player1[1]
+    p2s = player2[1]
+    
+=begin
+    # Validate input
+    if p1s != 'R' || p1s != 'P' || p1s != 'S'
+      # raise RockPaperScissors::NoSuchStrategyError 'Strategy must be one of R,P,S'
+      puts "one"
+    elsif p1s != 'R' || p1s != 'P' || p1s != 'S'
+      #raise RockPaperScissors::NoSuchStrategyError 'Strategy must be one of R,P,S'
+      puts "2"
+    end
+=end
+    
+    if p1s == 'R' # If player1 plays R
+      if p2s == 'R'
+        return player1
+      elsif p2s == 'P'
+        return player2
+      elsif p2s == 'S'
+        return player1
+      end      
+    elsif p1s == 'P' # If player2 plays P
+      if p2s == 'R'
+        return player1
+      elsif p2s == 'P'
+        return player1
+      elsif p2s == 'S'
+        return player2     
+      end
+    elsif p1s == 'S' # If player2 plays S
+      if p2s == 'R'
+        return player2
+      elsif p2s == 'P'
+        return player1
+      elsif p2s == 'S'
+        return player1
+      end     
+    end
+    
   end
 
   def self.tournament_winner(tournament)
