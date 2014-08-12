23a03169e3a281ca781602044a0351d686b17665
Jeremy <jeremy@kwmassage.com>
2014-08-12 10:05:40 -0300

    another try 1-w

--- hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -1,28 +1,15 @@
 class Class
-  def attr_accessor_with_history(attr_name)
+  def self.attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     self.class_eval %Q{
 
       def #{attr_name}=(val)
-        if !defined?@#{attr_name}
-          @#{attr_name}_hisory ||= [nil]          
-          if defined? @last_val
-            @#{attr_name}_hisory << @last_val
-          end
-          @#{attr_name} = val
-          puts @#{attr_name}.inspect
-          puts @#{attr_name}_hisory.inspect
-        else          
-          @#{attr_name}_hisory << val 
-          puts @#{attr_name}.inspect
-          puts @#{attr_name}_hisory.inspect
-        end
-        def #{attr_name}_history
-          @#{attr_name}_history
-        end
-        @last_val = val
+        @#{attr_name}_history ||= Array.new
+        @#{attr_name}_history << val
+        
+        @#{attr_name} = val
       end		      
     }
   end
