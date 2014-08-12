165788c1a33dce47318d7626a6ecb55a35c994f3
Jeremy <jeremy@kwmassage.com>
2014-08-12 15:39:18 -0400

    delete

--- hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -3,8 +3,22 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
+    @history_variable = {'foo' => attr_name+"_history"}
+    puts value
+    
     class_eval %Q{
-      # YOUR CODE HERE
+
+      if !defined hist
+        @attr_name+"_history"[]
+        @attr_name+"_history" << value
+      else
+        @attr_name+"_history" << value
+      end
+      puts @attr_name+"_history".inspect
+   
     }
   end
 end
+
+git log
+
