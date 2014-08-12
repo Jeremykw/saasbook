28272d1c277c40c862214e9112bcc562705a12fb
Jeremy <jeremy@kwmassage.com>
2014-08-11 23:04:16 -0300

    hw1-4, working on remember history seperatley

--- hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -3,8 +3,27 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval %Q{
-      # YOUR CODE HERE
+    self.class_eval %Q{
+
+      def #{attr_name}=(val)
+        if !defined?@#{attr_name}
+          @#{attr_name}_hisory ||= [nil]          
+          if defined? @last_val
+            @#{attr_name}_hisory << @last_val
+          end
+          @#{attr_name} = val
+          puts @#{attr_name}.inspect
+          puts @#{attr_name}_hisory.inspect
+        else          
+          @#{attr_name}_hisory << val 
+          puts @#{attr_name}.inspect
+          puts @#{attr_name}_hisory.inspect
+        end
+        def #{attr_name}_history
+          @#{attr_name}_history
+        end
+        @last_val = val
+      end		      
     }
   end
 end
