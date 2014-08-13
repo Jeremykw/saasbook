c8689b8c26d5be84f30479592bcb70fbe71d6c95
Jeremy <jeremy@kwmassage.com>
2014-08-12 21:21:23 -0400

    start rotten potates

---
+++ myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '75a62b511f041f8e0c09d0d2f7f7969326298421b7c6a8f12d437c4546d941c2b4d737d2adbace2a933814106a9fe01c0d4f7bd61dd1961b92ee07667ca3c084'
