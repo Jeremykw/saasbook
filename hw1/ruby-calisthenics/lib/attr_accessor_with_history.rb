6e5b8d2c492379b741c9e20e64c7d3ddf7062d11
Jeremy <jeremy@kwmassage.com>
2014-08-12 21:19:10 -0400

    start rottanpotatoes

--- hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -3,12 +3,12 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-
+    
       class_eval %Q{
-        
+
         def #{attr_name}= (val)
-         @#{attr_name}_history ||= [nil]
-         @#{attr_name}_history[1] = val
+         @@#{attr_name}_history ||= [nil]
+         @@#{attr_name}_history[1] = val
          @#{attr_name} = val
          
           
